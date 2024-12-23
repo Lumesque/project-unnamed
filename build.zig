b482782c8af2c104d6d0d542398cd21f1300c04c
Joshua Luckie <luckie.joshua.c@gmail.com>
2024-12-22 23:49:49 -0500

    Adding in example of how to compile c++ library

--- build.zig
+++ build.zig
@@ -15,26 +15,24 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "project-unnamed",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
-
     const exe = b.addExecutable(.{
         .name = "project-unnamed",
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = null,
         .target = target,
         .optimize = optimize,
     });
+
+    const flags = [_][]const u8{
+        "-pedantic-errors",
+        "-Wall",
+        "-Werror",
+        "-std=c++23",
+    };
+
+    const cexe: std.Build.Module.CSourceFile = .{ .file = b.path("src/c++/main.cpp"), .flags = &flags };
+
+    exe.addCSourceFile(cexe);
+    exe.linkLibCpp();
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
---
+++ src/c++/main.cpp
@@ -0,0 +1,32 @@
+/**
+* This is an introduction into how doxygen comments will be generated.
+*
+*/
+#include <iostream>
+
+/** A test class with an example description */
+/*! A more elaborate class description. */
+class ExampleClass {
+    /** Class constructor
+     *  Some form of a description
+     */
+    ExampleClass() = default;
+    /** Class destructor
+     *  Some form of a description
+     */
+    ~ExampleClass() = default;
+    /** A normal member taking two arguments and returning an integer value. */
+    /*!
+     * @param a an integer value
+     * @param s a constant character pointer
+     * @return The test result
+     * \sa ExampleClass(), ~ExampleClass()
+     */
+    int testMe(int a, const char *s) { return 0; };
+};
+
+int main(int argc /**< [in] docs for input parameter. */, const char** argv) {
+    int x = 1; /**< Description of member variable */
+    std::cout << x << " " << "Hello, world!\n";
+    return 0;
+}
